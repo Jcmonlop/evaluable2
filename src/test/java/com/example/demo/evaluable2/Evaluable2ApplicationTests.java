06da01c5406a478b929e6b35e941a1a2cf542d08
jcmonlop <jcmonlop95@gmail.com>
2023-10-10 13:26:02 +0200

    Evaluación 2

---
+++ src/test/java/com/example/demo/evaluable2/Evaluable2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.evaluable2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluable2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
